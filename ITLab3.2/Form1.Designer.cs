483bfedca896f780d13d5c8d9c62bf3cffd264ca
glauzi <glauzibusiness@gmail.com>
2025-10-07 20:34:52 +0700

    Добавьте файлы проекта.

---
+++ ITLab3.2/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ITLab3._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.open = new System.Windows.Forms.Button();
+            this.files = new System.Windows.Forms.ListBox();
+            this.player = new AxWMPLib.AxWindowsMediaPlayer();
+            this.playAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // open
+            // 
+            this.open.Location = new System.Drawing.Point(35, 44);
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(75, 23);
+            this.open.TabIndex = 0;
+            this.open.Text = "Открыть";
+            this.open.UseVisualStyleBackColor = true;
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // files
+            // 
+            this.files.FormattingEnabled = true;
+            this.files.Location = new System.Drawing.Point(35, 73);
+            this.files.Name = "files";
+            this.files.Size = new System.Drawing.Size(217, 277);
+            this.files.TabIndex = 1;
+            // 
+            // player
+            // 
+            this.player.Enabled = true;
+            this.player.Location = new System.Drawing.Point(258, 12);
+            this.player.Name = "player";
+            this.player.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("player.OcxState")));
+            this.player.Size = new System.Drawing.Size(530, 338);
+            this.player.TabIndex = 2;
+            // 
+            // playAll
+            // 
+            this.playAll.Location = new System.Drawing.Point(140, 44);
+            this.playAll.Name = "playAll";
+            this.playAll.Size = new System.Drawing.Size(112, 23);
+            this.playAll.TabIndex = 3;
+            this.playAll.Text = "Воспроизведение";
+            this.playAll.UseVisualStyleBackColor = true;
+            this.playAll.Click += new System.EventHandler(this.playAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 671);
+            this.Controls.Add(this.playAll);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.files);
+            this.Controls.Add(this.open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button open;
+        private System.Windows.Forms.ListBox files;
+        private AxWMPLib.AxWindowsMediaPlayer player;
+        private System.Windows.Forms.Button playAll;
+    }
+}
+
---
+++ ITLab3.2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITLab3._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "Video files (*.wmv;*.avi;*.mp4)|*.wmv;*.avi;*.mp4|All files (*.*)|*.*";
+            dialog.FilterIndex = 1;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                var filename = dialog.FileName;
+                files.Items.Add(filename);
+            }
+        }
+
+        private void playAll_Click(object sender, EventArgs e)
+        {
+            if (files.Items.Count == 0)
+            {
+                MessageBox.Show("Сначала добавьте файлы через кнопку 'Открыть'");
+                return;
+            }
+            var playlist = player.newPlaylist("MyPlaylist", "");
+
+            foreach (var selected in files.Items)
+            {
+                string filename = selected.ToString();
+                var mediaItem = player.newMedia(filename);
+                playlist.appendItem(mediaItem);
+            }
+
+            player.currentPlaylist = playlist;
+            player.Ctlcontrols.play();
+        }
+    }
+}
